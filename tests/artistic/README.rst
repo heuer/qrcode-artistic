086aa639c85faaad234fd5f34db809dc311bb1a4
Lars <heuer@semagia.com>
2020-09-09 19:29:49 +0200

    Better coverage

---
+++ tests/artistic/README.rst
@@ -0,0 +1,16 @@
+animated.gif
+============
+
+Source: <https://www.animatedimages.org/img-animated-cat-image-0072-58439.htm>
+
+
+transparency.png
+================
+
+Source: <https://commons.wikimedia.org/wiki/Category:Portable_Network_Graphics?uselang=de#/media/File:PNG_transparency_demonstration_1.png>
+
+
+sunflower.jpg
+=============
+
+Source: <https://commons.wikimedia.org/wiki/Category:JPEG_file_format?uselang=de#/media/File:Sunflower_as.jpg>
---
+++ tests/test_to_artistic.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 - 2020 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# All rights reserved.
+#
+# License: BSD License
+#
+"""\
+Tests against QRCode.to_artistic
+"""
+from __future__ import absolute_import
+import os
+import tempfile
+from PIL import Image
+import pytest
+import segno
+
+
+def _img_src(name):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'artistic', name)
+
+
+def _make_tmp_filename(ext):
+    f = tempfile.NamedTemporaryFile('wb', suffix='.' + ext, delete=False)
+    print(f.name)
+    return f.name
+
+
+def test_animated():
+    qr = segno.make_qr('A')
+    scale = 4
+    width, height = qr.symbol_size(scale=scale)
+    fn = _make_tmp_filename('gif')
+    qr.to_artistic(_img_src('animated.gif'), fn, scale=scale)
+    img = Image.open(fn)
+    try:
+        assert (width, height) == img.size
+        assert img.is_animated
+    finally:
+        os.remove(fn)
+
+
+def test_transparency():
+    qr = segno.make_qr('A')
+    scale = 9
+    width, height = qr.symbol_size(scale=scale)
+    fn = _make_tmp_filename('png')
+    qr.to_artistic(_img_src('transparency.png'), fn, scale=scale)
+    img = Image.open(fn)
+    try:
+        assert (width, height) == img.size
+        assert not img.is_animated
+    finally:
+        os.remove(fn)
+
+
+def test_jpeg():
+    qr = segno.make_qr('A')
+    scale = 27
+    width, height = qr.symbol_size(scale=scale)
+    fn = _make_tmp_filename('jpg')
+    qr.to_artistic(_img_src('sunflower.jpg'), fn, scale=scale)
+    img = Image.open(fn)
+    try:
+        assert (width, height) == img.size
+    finally:
+        os.remove(fn)
+
+
+def test_jpeg_to_png():
+    qr = segno.make_qr('A')
+    scale = 5
+    width, height = qr.symbol_size(scale=scale)
+    fn = _make_tmp_filename('png')
+    qr.to_artistic(_img_src('sunflower.jpg'), fn, scale=scale)
+    img = Image.open(fn)
+    try:
+        assert (width, height) == img.size
+    finally:
+        os.remove(fn)
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
